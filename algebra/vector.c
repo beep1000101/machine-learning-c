a80ef6e2f4ff585b064cf41b0602895c20c8f0fb
Matt <matiponiatowski@o2.pl>
2026-07-08 19:45:21 +0200

    Refactor vector operations by adding print_vector and free_vector functions for improved code organization

--- algebra/vector.c
+++ algebra/vector.c
@@ -6,6 +6,12 @@ typedef struct{
     double* data;
 } Vector;
 
+void print_vector(Vector vector) {
+    for (size_t i = 0; i < vector.size; i++) {
+        printf("%f\n", vector.data[i]);
+    }
+}
+
 Vector vector_create(size_t size) {
     Vector vector;
     vector.size = size;
@@ -22,16 +28,33 @@ Vector vector_from_array(const double *data, size_t size) {
     return vector;
 }
 
-int main(){
-    size_t size = 3;
-    double input[] = {1.0, 3.0, 7.0};
-    Vector vector = vector_from_array(input, size);
+void free_vector(Vector vector) {
+    free(vector.data);
+}
 
-    for (size_t i = 0; i < vector.size; i++) {
-        printf("%f\n", vector.data[i]);
+Vector add_vector(Vector vector_left, Vector vector_right) {
+    Vector result = vector_create(vector_left.size);
+    for (int i = 0; i < vector_left.size; i++){
+        result.data[i] = vector_left.data[i] + vector_right.data[i];
     }
 
+    return result;
+}
 
-    free(vector.data);
+int main(){
+    size_t size = 3;
+    double input1[] = {1.0, 3.0, 7.0};
+    Vector vector1 = vector_from_array(input1, size);
+    double input2[] = {-1.0, 2.0, 0.0};
+    Vector vector2 = vector_from_array(input2, size);
+
+    Vector sum_of_vectors = add_vector(vector1, vector2);
+
+    printf("sum of vectors:\n");
+    print_vector(sum_of_vectors);
+
+
+    free_vector(vector1);
+    free_vector(vector2);
     return 0;
 }
