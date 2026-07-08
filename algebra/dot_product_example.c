15fbe6ba206ea5de8f4415f26711fa4ea0f5821b
Matt <matiponiatowski@o2.pl>
2026-07-08 20:07:29 +0200

    Refactor formatting in dot_product_example.c for improved readability

--- algebra/dot_product_example.c
+++ algebra/dot_product_example.c
@@ -10,16 +10,12 @@ struct Matrix {
     float elements[Dimension][Dimension];
 };
 
-void multiply_matrix_vector(
-    const struct Matrix *matrix,
-    const struct Vector *vector,
-    struct Vector *result
-) {
+void multiply_matrix_vector(const struct Matrix *matrix, const struct Vector *vector,
+                            struct Vector *result) {
     for (int row = 0; row < Dimension; row++) {
         result->elements[row] = 0.0f;
         for (int column = 0; column < Dimension; column++) {
-            result->elements[row] +=
-                matrix->elements[row][column] * vector->elements[column];
+            result->elements[row] += matrix->elements[row][column] * vector->elements[column];
         }
     }
 }
@@ -33,21 +29,14 @@ float dot_product(const struct Vector *left, const struct Vector *right) {
 }
 
 int main(void) {
-    struct Matrix matrix = {
-        .elements = {
-            {1, 0, 0},
-            {-1, 1, 1},
-            {-1, 0, 1}
-        }
-    };
+    struct Matrix matrix = {.elements = {{1, 0, 0}, {-1, 1, 1}, {-1, 0, 1}}};
 
     struct Vector vector = {.elements = {1, 3, 1}};
     struct Vector vector2 = {.elements = {-1, 2, 2}};
     struct Vector result;
 
     multiply_matrix_vector(&matrix, &vector, &result);
-    printf("[%f, %f, %f]\n",
-           result.elements[0], result.elements[1], result.elements[2]);
+    printf("[%f, %f, %f]\n", result.elements[0], result.elements[1], result.elements[2]);
 
     float dot = dot_product(&vector, &vector2);
     printf("%f\n", dot);
