804561461b3b2fa0d657efce28d7fb3599ec4e4a
Matt <matiponiatowski@o2.pl>
2026-07-08 20:00:07 +0200

    Implement scalar multiplication and dot product for vectors; update main to demonstrate functionality

--- algebra/vector.c
+++ algebra/vector.c
@@ -32,6 +32,14 @@ void free_vector(Vector vector) {
     free(vector.data);
 }
 
+Vector scalar_mul(Vector vector, int scalar) {
+    Vector result = vector_create(vector.size);
+    for (int i = 0; i < vector.size; i++){
+        result.data[i] = vector.data[i] * scalar;
+    }
+    return result;
+}
+
 Vector add_vector(Vector vector_left, Vector vector_right) {
     Vector result = vector_create(vector_left.size);
     for (int i = 0; i < vector_left.size; i++){
@@ -41,7 +49,25 @@ Vector add_vector(Vector vector_left, Vector vector_right) {
     return result;
 }
 
+Vector sub_vector(Vector vector_left, Vector vector_right) {
+    Vector result = vector_create(vector_left.size);
+    for (int i = 0; i < vector_left.size; i++){
+        result.data[i] = vector_left.data[i] - vector_right.data[i];
+    }
+
+    return result;
+}
+
+double dot(Vector vector_left, Vector vector_right) {
+    double result = 0;
+    for (int i = 0; i < vector_left.size; i++){
+        result += vector_left.data[i] * vector_right.data[i];
+    }
+    return result;
+}
+
 int main(){
+    int scalar = 3;
     size_t size = 3;
     double input1[] = {1.0, 3.0, 7.0};
     Vector vector1 = vector_from_array(input1, size);
@@ -49,12 +75,20 @@ int main(){
     Vector vector2 = vector_from_array(input2, size);
 
     Vector sum_of_vectors = add_vector(vector1, vector2);
+    Vector scalar_multiplied_vector = scalar_mul(vector1, scalar);
+    double dot_product = dot(vector1, vector2);
 
     printf("sum of vectors:\n");
     print_vector(sum_of_vectors);
+    printf("vector1 times %d:\n", scalar);
+    print_vector(scalar_multiplied_vector);
+    printf("vector1 dot vector2\n");
+    printf("%f\n", dot_product);
 
 
     free_vector(vector1);
     free_vector(vector2);
+    free_vector(sum_of_vectors);
+    free_vector(scalar_multiplied_vector);
     return 0;
 }
